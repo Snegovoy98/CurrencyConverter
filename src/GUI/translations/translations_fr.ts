a8c5ea8d3e2800a2d0ca8e8d86abeb3c495d6bd4
Alex <Alex>
2024-09-25 20:55:00 +0300

    There was added translations funtional

---
+++ src/GUI/translations/translations_fr.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>Main</name>
+    <message>
+        <location filename="../CurrencyConverterModules/Main.qml" line="9"/>
+        <source>Currency Converter</source>
+        <translation>Convertisseur de devises</translation>
+    </message>
+</context>
+</TS>
---
+++ src/GUI/translations/translations_it.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>Main</name>
+    <message>
+        <location filename="../CurrencyConverterModules/Main.qml" line="9"/>
+        <source>Currency Converter</source>
+        <translation>Convertitore di valuta</translation>
+    </message>
+</context>
+</TS>
